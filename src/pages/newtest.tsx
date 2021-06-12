da562e0a9b4620d89043fff59f2d5196a03dd5c9
kaito hasegawa <kaitohasegawa000@gmail.com>
2021-06-12 17:50:30 +0900

    修正

--- src/pages/newtest.tsx
+++ src/pages/newtest.tsx
@@ -371,7 +371,7 @@ const NewTest: React.FC = () => {
           </div>
           <div className="page2_message_4_iframe">
             <span>
-              <iframe width="560" height="315" src="https://www.youtube.com/embed/b_aTGKeWGA0" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"  className="iframe"></iframe>
+              <iframe width="560" height="315" src="https://www.youtube.com/embed/b_aTGKeWGA0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"  className="iframe"></iframe>
             </span>
           </div>
 
@@ -381,7 +381,7 @@ const NewTest: React.FC = () => {
             </div>
           </div>
         <div className="page2_message_4_iframe">
-          <iframe width="560" height="315" src="https://www.youtube.com/embed/X3ife-OcREI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"  className="iframe"></iframe>
+          <iframe width="560" height="315" src="https://www.youtube.com/embed/X3ife-OcREI" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"  className="iframe"></iframe>
         </div>
           
           <div className="page4_message_1">
